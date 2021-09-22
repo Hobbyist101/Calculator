4600c8888af18e9f01525091d1ed76c709073f74
hobbysit101 <dontknow.knived@simplelogin.co>
2021-09-22 22:30:36 +0800

    Fix Typo

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let number = document.querySelector('.firstDisplay');
-let oldNumber = document.querySelector('.secondDisplay');
-let calculation = 0;
+const number = document.querySelector('.firstDisplay');
+const oldNumber = document.querySelector('.secondDisplay');
 
 let plus = false;
 let minus = false;
@@ -10,32 +9,25 @@ let enter = false;
 let in1 = false;
 let in2 = false;
 
+let calculation = 0;
 let input1 = "";
 let input2 = "";
 let symbol = "";
 
-function addFunc(min,max) {
-    const input1 = Number(min);
-    const input2 = Number(max);
-	return input1 + input2
+function addFunc(input1,input2) {
+	return (Number(input1) + Number(input2));
 };
 
-function subtractFunc(min,max) {
-	const input1 = Number(min);
-    const input2 = Number(max);
-	return input1 - input2;
+function subtractFunc(input1,input2) {
+	return (Number(input1) - Number(input2));
 };
 
-function multiplyFunc(min,max) {
-	const input1 = Number(min);
-    const input2 = Number(max);
-	return input1 * input2;
+function multiplyFunc(input1,input2) {
+	return (Number(input1) * Number(input2));
 };
 
-function divideFunc(min,max) {
-	const input1 = Number(min);
-    const input2 = Number(max);
-	return input1 / input2;
+function divideFunc(input1,input2) {
+	return (Number(input1) / Number(input2));
 };
 
 function button(num){
@@ -72,14 +64,11 @@ function backspaceFunction(){
     let array = [...number.textContent];
     array.pop();
     for (let i = 0; i < array.length; i++){
-        function sliceFunction(i){
+        if (array[i] != 0 && array[i] == "-" || array[i] == "+" || array[i] == "*" || array[i] == "/"){
             sliced1 = array.slice(0,i);
             sliced2 = array.slice(i+1);
             input1 = sliced1.join('');
             input2 = sliced2.join(''); 
-        }
-        if (array[i] != 0 && array[i] == "-" || array[i] == "+" || array[i] == "+" || array[i] == "/"){
-            sliceFunction(i);
         }  
     }
     if (input2 != ""){
@@ -129,87 +118,6 @@ function clearFunction(){
     input2 = "";
 }
 
-plusBtn.addEventListener('click', () => {
-    symbolBtn("+",plus);
-    plus = true;
-})
-
-minusBtn.addEventListener('click', () => {
-    symbolBtn("-",minus);
-    minus = true; 
-})
-
-multiplyBtn.addEventListener('click', () => {
-    symbolBtn("*",multiply);
-    multiply = true;
-})
-
-divideBtn.addEventListener('click', () => {
-    symbolBtn("/",divide);
-    divide = true;
-})
-
-zeroBtn.addEventListener('click', () => {
-    button('0');
-})
-
-oneBtn.addEventListener('click', () => {
-    button('1');
-})
-
-twoBtn.addEventListener('click', () => {
-    button('2');
-})
-
-threeBtn.addEventListener('click', () => {
-    button('3');
-})
-
-fourBtn.addEventListener('click', () => {
-    button('4');
-})
-
-fiveBtn.addEventListener('click', () => {
-    button('5');
-})
-
-sixBtn.addEventListener('click', () => {
-    button('6');
-})
-
-sevenBtn.addEventListener('click', () => {
-    button('7');
-})
-
-eightBtn.addEventListener('click', () => {
-    button('8');
-})
-
-nineBtn.addEventListener('click', () => {
-    button('9');
-})
-
-dotBtn.addEventListener('click', () => {
-    button('.');
-})
-
-backspaceBtn.addEventListener('click', () => {
-    backspaceFunction();
-})
-
-enterBtn.addEventListener('click', () => {
-    enterFunction();
-})
-
-clearBtn.addEventListener('click', () => {
-    clearFunction();
-})
-
-clearAllBtn.addEventListener('click', () => {
-    clearFunction();
-    oldNumber.textContent = 0;
-})
-
 window.addEventListener('keydown', (keyDown) => {
     const keyCode = keyDown.key;
     const keyString = keyCode.toString();
@@ -236,3 +144,32 @@ window.addEventListener('keydown', (keyDown) => {
         clearFunction();
     } 
 })
+
+window.addEventListener('click', (click) => {
+    const clickBtn = click.target;
+    if (clickBtn.dataset.number >= 0 && clickBtn.dataset.number <= 9 || clickBtn.dataset.number == "."){
+        button(clickBtn.dataset.number);
+    } else if (clickBtn.dataset.key == "+"){
+        plus = true;
+        symbolBtn("+",plus);
+    } else if (clickBtn.dataset.key == "-"){
+        minus = true; 
+        symbolBtn("-",minus);
+    } else if (clickBtn.dataset.key == "*"){
+        multiply = true;
+        symbolBtn("*",multiply);
+    }   else if (clickBtn.dataset.key == "/"){
+        divide = true;
+        symbolBtn("/",divide);
+    } else if (clickBtn.dataset.key == "enter"){
+        enterFunction(); 
+    } else if (clickBtn.dataset.key == "backspace"){
+        backspaceFunction();
+    } else if (clickBtn.dataset.key == "clear"){
+        clearFunction();
+    } else if (clickBtn.dataset.key == "clearall"){
+        clearFunction();
+        oldNumber.textContent = 0;
+    }
+})
+
